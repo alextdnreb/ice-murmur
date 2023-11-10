3f3d64a26829871924cc30a51518edf849f4b244
Alexander <alex.berndt95@web.de>
2023-11-10 20:56:09 +0100

    push

--- ice.ts
+++ ice.ts
@@ -4,13 +4,17 @@ import { Murmur } from "./generated/Murmur";
 (async () => {
   let communicator: Ice.Communicator;
   try {
-    communicator = Ice.initialize();
+    const props = Ice.createProperties()
+    props.setProperty("Ice.ImplicitContext", "Shared")
+    const data = new Ice.InitializationData()
+    data.properties = props
+    communicator = Ice.initialize(data);    
     const proxy = communicator.stringToProxy("Meta:tcp -h localhost -p 6502");
+    communicator.getImplicitContext().put("secret", "asd")
     const meta = await Murmur.MetaPrx.checkedCast(proxy);
-    // const server = await meta.newServer();
-    // server.start();
-
-    // console.log(server.id());
+    const server = await meta.newServer()
+    await server.start()
+    console.log(await server.id())
   } catch (ex) {
     console.log(ex.toString());
   } finally {
